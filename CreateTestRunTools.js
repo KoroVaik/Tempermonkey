1b45ace7fba7e9a2b9294c570d6ab2f2ef74859f
KoroVaik <52558615+KoroVaik@users.noreply.github.com>
2025-12-30 22:24:08 +0200

    Bump version to 7.3 in CreateTestRunTools.js

--- CreateTestRunTools.js
+++ CreateTestRunTools.js
@@ -2,7 +2,7 @@
 // @name          Azure DevOps Release Category Filter Popup - Enhanced
 // @description   Prepare a filter for the test run; Deselects all active stages in the release
 // @namespace     http://tampermonkey.net/
-// @version       7.2
+// @version       7.3
 // @match         https://dev.azure.com/*
 // @downloadURL   https://raw.githubusercontent.com/KoroVaik/Tempermonkey/refs/heads/main/CreateTestRunTools.js
 // @updateURL     https://raw.githubusercontent.com/KoroVaik/Tempermonkey/refs/heads/main/CreateTestRunTools.js
